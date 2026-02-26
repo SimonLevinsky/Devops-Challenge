387e9c6e05ee8fcd898a91ce335b8a30541b99c6
SimonDevops <levinskysimon7@gmail.com>
2026-02-26 19:03:16 +0200

    Fix: correct HelloController formatting

--- app/src/main/java/com/devops/app/HelloController.java
+++ app/src/main/java/com/devops/app/HelloController.java
@@ -1,12 +1,13 @@
-Javapackage com.devops.app;
+package com.devops.app;
 
-      import org.springframework.web.bind.annotation.GetMapping;
-      import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-      @RestControllerpublic
-      class HelloController { 
-        @GetMapping("/")
-          public String index() { 
-                return "200 OK - Application is running!"; 
-                }
-        }
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "200 OK - Application is running!";
+    }
+}
